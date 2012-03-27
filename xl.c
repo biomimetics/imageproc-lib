e260bf9923dbe17a6e694f74698fce88f63dc771
Stanley S. Baek <stanbaek@gmail.com>
2012-03-26 23:00:56 -0700

    Added Analog Devices ADXL345 accelerometer driver.

---
+++ xl.c
@@ -0,0 +1,370 @@
+/*
+ * Copyright (c) 2010, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California, Berkeley nor the names
+ *   of its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Analog Devices ADXL345 3-axis MEMS accelerometer IC Interface
+ *
+ * by Stanley S. Baek
+ *
+ * v.beta
+ *
+ * Revisions:
+ *  Stanley S. Baek      2010-06-05    Initial release
+ *                      
+ * Notes:
+ *  - Uses an I2C port for communicating with the accelerometer chip
+ *  - MCU resources requied for this module:
+ *      I2C bus (I2C1 for ImageProc2) - SCL1 & SDA1
+ *      External INT (INT3 for ImageProc2)
+ */
+
+
+#include "ports.h"      // for external interrupt
+#include "i2c.h"
+#include "xl.h"
+#include "utils.h"
+
+#define XL_ADDR_RD             0xA7
+#define XL_ADDR_WR             0xA6
+#define XL_DEFAULT_SCALE       0.03832  // = 9.81/256
+
+#define xlReadString(a,b,c) MastergetsI2C1(a,b,c)
+
+
+/*-----------------------------------------------------------------------------
+ *          Static Variables
+-----------------------------------------------------------------------------*/
+// data storage for receiving data
+static union {
+    unsigned char chr_data[6];
+    int int_data[3];
+} XlData;
+
+
+static union {
+    unsigned char chr_data[24];
+    float f_data[6];
+} CalibParam;
+
+
+/*-----------------------------------------------------------------------------
+ *          Declaration of static functions
+-----------------------------------------------------------------------------*/
+static void xlWrite(unsigned char regaddr, unsigned char data);
+static inline void xlSendByte( unsigned char byte);
+static inline unsigned char xlReceiveByte(void);
+static inline void xlSendNACK(void);
+static inline void xlStartTx(void);
+static inline void xlEndTx(void);
+static inline void xlSetupPeripheral(void);
+
+
+/*-----------------------------------------------------------------------------
+ *          Public functions
+-----------------------------------------------------------------------------*/
+
+void xlSetup(void) {
+
+    xlSetupPeripheral();
+        
+    ConfigINT3(RISING_EDGE_INT & EXT_INT_DISABLE & EXT_INT_PRI_3); 
+
+    delay_ms(25);   // power up delay, may not need...
+
+    // Do not use offset registers, but use calib_param_
+    xlWrite(0x1e, 0x00); // x-axis offset is zero
+    xlWrite(0x1f, 0x00); // y-axis offset is zero
+    xlWrite(0x20, 0x00); // z-axis offset is zero
+
+    xlWrite(0x21, 0x00); // disable the single/double tap functions
+    xlWrite(0x22, 0x00); // disable the double tap function
+    xlWrite(0x23, 0x00); // disable the double tap function
+    xlWrite(0x27, 0x00); // disable the activity/inactivity
+    xlWrite(0x2a, 0x00); // disable single/double tap fuctions
+
+    xlWrite(0x2c, 0x0d); // normal power mode & 800Hz output rate
+    xlWrite(0x2d, 0x08); // normal mode 
+
+    //interrupt enable/disable
+    xlWrite(0x2e, 0x80); // DATA_READY is enabled. others are disabled
+    xlWrite(0x2f, 0x00); // all interrputs are sent to INT1 Pin
+
+    // data format
+    // enable the self-test force
+    // active high for interrupts
+    // full resolution mode
+    // right justified mode
+    // +-8g range
+    xlWrite(0x31, 0b00001010);   
+	//xlWrite(0x31, 0b00001011);  //Same as above, but +-16g range
+
+    xlWrite(0x38, 0x00); // FIFO control is bypassed
+
+    // set default scale factors
+    CalibParam.f_data[0] = XL_DEFAULT_SCALE;
+    CalibParam.f_data[1] = XL_DEFAULT_SCALE;
+    CalibParam.f_data[2] = XL_DEFAULT_SCALE;
+
+}
+
+
+void xlSetIntEn(unsigned char flag) {
+    _INT3IE = flag;
+}
+
+
+void xlSetRange(unsigned char range) {
+    if (range == 2) {
+        xlWrite(0x31, 0b00001000); 
+    } else if (range == 4) {
+        xlWrite(0x31, 0b00001001); 
+    } else if (range == 8) {
+        xlWrite(0x31, 0b00001010); 
+    } else if (range == 16) {
+        xlWrite(0x31, 0b00001011); 
+    }
+    // else do not change anything
+}
+
+
+void xlSetOutputRate(unsigned char power_mode, unsigned char rate) {
+    if (power_mode == 0) {
+        xlWrite(0x2c, (0x0f & rate));
+    } else {
+        xlWrite(0x2c, ((0x0f & rate) | 0x10));
+    }
+}
+
+
+void xlSleep(void) {
+    xlWrite(0x2d, 0x00);    // stanby mode, clear Measure bit
+}
+
+
+void xlWake(void) {
+    xlWrite(0x2d, 0x08);    // normal measurement mode, set Measure bit
+}
+
+void xlSetCalibParam(unsigned char* param) {
+    unsigned char i;
+
+    for(i = 0; i < 24; ++i) {
+        CalibParam.chr_data[i] = param[i];
+    }
+}
+
+unsigned char* xlGetCalibParam(void) {
+    return CalibParam.chr_data;
+}
+
+
+void xlLoadCalibParam(void) {
+    // TODO: Implement !!!!!
+   // work harder !!
+}
+
+
+void xlSaveCalibParam(void){
+    // TODO: Implement !!!!!
+   // work harder !!
+}
+
+unsigned char xlGetID(void) {
+    unsigned char c;
+    xlStartTx();
+    xlSendByte(XL_ADDR_WR);
+    xlSendByte(0x00);
+    xlEndTx();
+    xlStartTx();
+    xlSendByte(XL_ADDR_RD);
+    c = xlReceiveByte();    
+    xlEndTx();
+    return c;
+}    
+
+
+void xlGetFloatXYZ(float* data){
+    data[0] = XlData.int_data[0]*CalibParam.f_data[0] + CalibParam.f_data[3];
+    data[1] = XlData.int_data[1]*CalibParam.f_data[1] + CalibParam.f_data[4];
+    data[2] = XlData.int_data[2]*CalibParam.f_data[2] + CalibParam.f_data[5];
+}
+
+int* xlGetIntXYZ(void){
+    return XlData.int_data;
+}
+
+unsigned char* xlToString(void) {
+    return XlData.chr_data;
+}
+
+void xlDumpData(unsigned char* buffer) {
+    int i;
+    for (i = 0; i < 6; i++) {
+        buffer[i] = XlData.chr_data[i];
+    }
+}
+
+
+unsigned char* xlReadXYZ(void)  {
+    xlStartTx();
+    xlSendByte(XL_ADDR_WR);
+    xlSendByte(0x32);
+    xlEndTx();
+    xlStartTx();
+    xlSendByte(XL_ADDR_RD);
+    xlReadString(6, XlData.chr_data, 1000);
+    xlEndTx();
+    return XlData.chr_data;
+
+}
+
+void xlGetXYZ(unsigned char *data)  {
+    xlStartTx();
+    xlSendByte(XL_ADDR_WR);
+    xlSendByte(0x32);
+    xlEndTx();
+    xlStartTx();
+    xlSendByte(XL_ADDR_RD);
+    xlReadString(6, data, 1000);
+    xlEndTx();
+  
+}
+
+
+
+
+/*-----------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------
+ * The functions below are intended for internal use, i.e., private methods.
+ * Users are recommended to use functions defined above.
+ * ----------------------------------------------------------------------------
+-----------------------------------------------------------------------------*/
+
+
+/**************************************************
+ * Interrupt hander for Accelerometer
+ * ************************************************/
+void __attribute__((interrupt, no_auto_psv)) _INT3Interrupt(void) {
+    
+    _INT3IF = 0;    // Clear the interrupt flag
+}
+
+/*****************************************************************************
+* Function Name : xlWrite
+* Description   : Write a data to a register
+* Parameters    : regaddr - address of register
+*                 data - value to be written to the register
+* Return Value  : None
+*****************************************************************************/
+static void xlWrite(unsigned char regaddr, unsigned char data ){
+    xlStartTx();
+    xlSendByte(XL_ADDR_WR);
+    xlSendByte(regaddr);
+    xlSendByte(data);
+    xlEndTx();
+}
+
+/*****************************************************************************
+* Function Name : xlSendByte
+* Description   : Send a byte to gyroscope
+* Parameters    : byte - a byte to send
+* Return Value  : None
+*****************************************************************************/
+static inline void xlSendByte( unsigned char byte ){
+    MasterWriteI2C1(byte);
+    while(I2C1STATbits.TRSTAT);
+    while(I2C1STATbits.ACKSTAT);
+}
+
+/*****************************************************************************
+* Function Name : xlReceiveByte
+* Description   : Receive a byte from gyroscope
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+static inline unsigned char xlReceiveByte(void){
+    return MasterReadI2C1();
+}
+
+/*****************************************************************************
+* Function Name : xlSendNACK
+* Description   : Send NACK to gyroscope
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+static inline void xlSendNACK(void){
+    NotAckI2C1();
+    while(I2C1CONbits.ACKEN);
+}
+
+/*****************************************************************************
+* Function Name : xlStartTx
+* Description   : Start I2C transmission
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+static inline void xlStartTx(void){
+    StartI2C1();
+    while(I2C1CONbits.SEN);
+}
+
+/*****************************************************************************
+* Function Name : xlEndTx
+* Description   : End I2C transmission
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+static inline void xlEndTx(void){
+    StopI2C1();
+    while(I2C1CONbits.PEN);
+}
+
+/******************************************************************************
+* Function Name : xlSetupPeripheral
+* Description   : This routine sets up I2C bus for this module
+* Parameters    : None
+* Return Value  : None
+*******************************************************************************/
+static inline void xlSetupPeripheral(void) {
+    unsigned int I2C1CONvalue, I2C1BRGvalue;
+    I2C1CONvalue = I2C1_ON & I2C1_IDLE_CON & I2C1_CLK_HLD &
+                   I2C1_IPMI_DIS & I2C1_7BIT_ADD & I2C1_SLW_DIS &
+                   I2C1_SM_DIS & I2C1_GCALL_DIS & I2C1_STR_DIS &
+                   I2C1_NACK & I2C1_ACK_DIS & I2C1_RCV_DIS &
+                   I2C1_STOP_DIS & I2C1_RESTART_DIS & I2C1_START_DIS;
+	
+    // BRG = Fcy(1/Fscl - 1/10000000)-1, Fscl = 400KHz
+    //Maximum ADC data rate of 800 Hz
+    I2C1BRGvalue = 95; 
+    OpenI2C1(I2C1CONvalue, I2C1BRGvalue);
+    IdleI2C1();
+}
+
+
+
+
---
+++ xl.h
@@ -0,0 +1,246 @@
+/*
+ * Copyright (c) 2010, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California, Berkeley nor the names
+ *   of its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Analog Devices ADXL345 3-axis MEMS accelerometer IC Interface
+ *
+ * by Stanley S. Baek
+ *
+ * v.beta
+ *
+ * Revisions:
+ *  Stanley S. Baek      2010-06-05    Initial release
+ *                      
+ * Notes:
+ *  - The default setup is +-8g and 800Hz output rate.
+ *  - Uses an I2C port for communicating with the accelerometer chip.
+ *  - MCU resources requied for this module:
+ *      I2C bus (I2C1 for ImageProc2) - SCL1 & SDA1
+ *      External INT (INT3)
+ *
+ * Usage:
+ *  #include  "xl.h"
+ *
+ *  float xlData[3];
+ *  unsigned char * xlStrData;
+ *
+ *  // initialize xl module with default range, +-8g, 800Hz output rate
+ *  xlSetup()
+ *
+ *  // change the operation range to +-2g
+ *  xlSetRange(2);
+ *  
+ *  // read out data from accelerometer and save in an internal buffer
+ *  xlReadXYZ();
+ *
+ *  // convert data into floating point values in m/s^2
+ *  xlGetfXYZ(xlData);
+ *     
+ *  // read the data in raw string format
+ *  // xlStrData[0] = lower byte of x-axis data
+ *  // xlStrData[1] = higher byte of x-axis data...
+ *  xlStrData = xlGetsXYZ();
+ *   
+ */
+
+#ifndef __XL_H
+#define __XL_H
+
+/*****************************************************************************
+* Function Name : xlSetup
+* Description   : Initialize accelerometer 
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+void xlSetup(void);
+
+/******************************************************************************
+* Function Name : xlSetIntEn
+* Description   : Enable or disable External Interrupt 3. It is disabled by
+*                 default. At this moment, there is no ISR for this module.
+*                 If we think we need any interrupt, we will implement 
+*                 xlHandleISR() function.
+* Parameters    : 1 (TRUE) to enable and 0 (FALSE) to disable
+* Return Value  : None
+******************************************************************************/
+void xlSetIntEn(unsigned char flag);
+
+/*****************************************************************************
+* Function Name : xSetRange
+* Description   : ChangePut accelerometer into sleep mode
+*                 It is recommended to use this function to save power if a 
+*                 user is not using acceleromter.
+* Parameters    : Range can be 2, 4, 8, or 16 for +-2g, +-4g, +-8g, +-16g, 
+*                 respectively. If a range value is other than those listed,
+*                 the current range will not be changed. The default range is
+*                 +-8g. The sensor resolution will be changed accordingly to 
+*                 maintain the full resolution. 
+* Return Value  : None
+*****************************************************************************/
+void xlSetRange(unsigned char range);
+
+/*****************************************************************************
+* Function Name : xlSetOutputRate
+* Description   : Set the output data rate. Please check the datasheet for 
+*                 ADXL345 page 13 for the valid rates.
+* Parameters    : power_mode is 0 for normal power consumption mode and 1 for 
+*                 low power consumption mode.
+*                 rate is the output data rate in the range of 0x00 - 0x0f.
+* Return Value  : None
+*****************************************************************************/
+void xlSetOutputRate(unsigned char power_mode, unsigned char rate); 
+
+/*****************************************************************************
+* Function Name : xlSleep
+* Description   : Put accelerometer into sleep mode
+*                 It is recommended to use this function to save power if a 
+*                 user is not using acceleromter.
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+void xlSleep(void);
+
+/*****************************************************************************
+* Function Name : xlWake
+* Description   : Put accelerometer into normal operating mode
+* Parameters    : None
+* Return Value  : None
+=*****************************************************************************/
+void xlWake(void);
+
+/*****************************************************************************
+* Function Name : xlSetCalibParam
+* Description   : Set the current calibration parameters with param
+* Parameters    : calibration parameters in string format.
+* Return Value  : None
+*****************************************************************************/
+void xlSetCalibParam(unsigned char* param);
+
+/*****************************************************************************
+* Function Name : xlGetCalibParam
+* Description   : Return the current calibration parameters
+* Parameters    : None
+* Return Value  : parameter data in string
+*****************************************************************************/
+unsigned char* xlGetCalibParam(void);
+
+/*****************************************************************************
+* Function Name : xlLoadCalibParam
+* Description   : Load the current calibration parameters from Flash Memory.
+*                 If the first scale factor is zero, use XL_DEFAULT_SCALE 
+*                 value for the first three parameters and use "0" for the
+*                 last three parameters.
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+void xlLoadCalibParam(void);
+
+/*****************************************************************************
+* Function Name : xlSaveCalibParam
+* Description   : Save the current calibration parameters in Flash Memory
+* Parameters    : None
+* Return Value  : None
+*****************************************************************************/
+void xlSaveCalibParam(void);
+
+/*****************************************************************************
+* Function Name : xlGetID
+* Description   : Get accelerometer identification number
+* Parameters    : None
+* Return Value  : Accelerometer ID
+*****************************************************************************/
+unsigned char xlGetID(void);
+
+/*****************************************************************************
+* Function Name : xlGetFloatXYZ
+* Description   : Get X, Y, and Z data in m/s^2 in floating point format.
+*                 This is the only function that returns calibrated sensor data
+*                 if the calibration parameters are set. 
+* Parameters    : Array of 3 floating point numbers to store accelerometer data.
+* Return Value  : None
+*****************************************************************************/
+void xlGetFloatXYZ(float* data);
+
+/*****************************************************************************
+* Function Name : xlGetIntXYZ
+* Description   : Get X, Y, and Z raw data in integer format. Users need to 
+*                 convert with a scale factor. For a fresh data, xlReadXYZ
+*                 must be called before this function call.
+* Parameters    : None
+* Return Value  : pointer for internal buffer holding 3 integers representing 
+*                 the 3-axis accelerometer data.
+*****************************************************************************/
+int* xlGetIntXYZ(void);
+
+/*****************************************************************************
+* Function Name : xlToString
+* Description   : Get X, Y, and Z raw data in string format. Users need to 
+*                 convert with a scale factor.  For a fresh data, xlReadXYZ
+*                 must be called before this function call.
+* Parameters    : None
+* Return Value  : The pointer of the internal buffer holding 6 characters 
+*                 representing the 3-axis accelerometer data.
+*****************************************************************************/
+unsigned char* xlToString(void);
+
+
+/*****************************************************************************
+* Function Name : xlDumpData
+* Description   : Get X, Y, and Z xl data in raw string format.
+* Parameters    : buffer is an char array holding 6 characters 
+*                 representing the 3-axis xl data.
+* Return Value  : None
+*****************************************************************************/
+void xlDumpData(unsigned char* buffer);
+
+
+/*****************************************************************************
+* Function Name : xlReadXYZ
+* Description   : This function reads X, Y, and Z xl outputs and save into 
+*                 an internal buffer and returns X, Y, and Z raw data in string
+*                 format. It will take about 250us for this function.
+* Parameters    : None
+* Return Value  : The pointer of the internal buffer holding 6 characters 
+*                 representing the 3-axis accelerometer data.
+*****************************************************************************/
+unsigned char*  xlReadXYZ(void);
+
+/*****************************************************************************
+* Function Name : xlGetXYZ
+* Description   : Read X, Y, and Z gyro outputs and save into the array passed 
+*                 into this function. xlReadXYZ does not have to be called for
+*                 this function call. It will take about 250us for this 
+*                 function.
+* Parameters    : An array of 6 characters to store 3-axis xl data. 
+* Return Value  : None
+*****************************************************************************/
+void xlGetXYZ(unsigned char *data);
+
+
+#endif // __XL_H
+
