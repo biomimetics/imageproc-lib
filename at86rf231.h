56e6a6586999dc8b15901054e1de0eb24d63aae4
Fernando L. Garcia Bermudez <fgb@eecs.berkeley.edu>
2012-03-27 01:55:55 -0700

    Updating some headers and adding Atmel's license.

--- at86rf231.h
+++ at86rf231.h
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2008 Atmel Corporation.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - The name of Atmel may not be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ * - This software may only be redistributed and used in connection with an
+ *   Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Driver for AT86RF231 transceiver.
+ */
 /**
  * @file at86rf231.h
  *
@@ -7,11 +38,6 @@
  *
  * @author    Atmel Corporation: http://www.atmel.com
  * @author    Support email: avr@atmel.com
- */
-/*
- * Copyright (c) 2008, Atmel Corporation All rights reserved.
- *
- * Licensed under Atmel's Limited License Agreement --> LICENSE.txt
  */
 
 /* Prevent double inclusion */
