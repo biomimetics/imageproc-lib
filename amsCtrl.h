73e4b79e0d1d68cb9bdc49827d36b0c312846092
dhaldane <dhaldane@berkeley.edu>
2012-10-22 16:14:40 -0700

    Tuned gains for ams pid

--- amsCtrl.h
+++ amsCtrl.h
@@ -6,11 +6,12 @@
 #define AMS_DEFAULT_KFF  0
 #define SOFT_GAIN_SCALER 512
 #elif defined PID_HARDWARE
-#define AMS_DEFAULT_KP  4000
-#define AMS_DEFAULT_KI  0
-#define AMS_DEFAULT_KD  100
+#define AMS_DEFAULT_KP  18000
+#define AMS_DEFAULT_KI  10
+#define AMS_DEFAULT_KD  0
 #define AMS_DEFAULT_KAW 0
 #define AMS_DEFAULT_KFF  0
+#define AMS_GAIN_SCALE  8
 #endif
 
 #define nPIDS  2
