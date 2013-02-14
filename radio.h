5f78a15f8ce0b3801e485bc0c8880cc20abdcccf
Andrew Pullin <pullin@berkeley.edu>
2013-02-13 17:20:21 -0800

    Update to radioSendData argument, as per suggestions from fgb.

--- radio.h
+++ radio.h
@@ -111,8 +111,8 @@ void radioSetWatchdogTime(unsigned int time);
 unsigned int radioEnqueueTxPacket(MacPacket packet);
 MacPacket radioDequeueRxPacket(void);
 
-char radioSendData(unsigned char* dataptr, unsigned int len,
-        unsigned char status, unsigned char type, unsigned int dest_addr);
+unsigned char radioSendData(unsigned int dest_addr, unsigned char status,
+        unsigned char type, unsigned int datalen, unsigned char* dataptr);
 
 unsigned int radioTxQueueEmpty(void);
 unsigned int radioTxQueueFull(void);
