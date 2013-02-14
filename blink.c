f53675d3d42dec724d3d1cfa19242732dd831494
Fernando L. Garcia Bermudez <fgb@eecs.berkeley.edu>
2013-02-13 23:57:00 -0800

    Minor cleanup of LED blink module
    Unsure if this needs to be in imageproc-lib or instead with the project.

--- blink.c
+++ blink.c
@@ -1,30 +1,61 @@
-/* blink.c 
-Ron Fearing Dec. 19, 2012
-make LEDs blink even if interrupts are over riding LED
-setting */
-// leave LEDs off when done 
+/*
+ * Copyright (c) 2012-2013, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the University of California, Berkeley nor the names
+ *   of its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Blink LEDs
+ *
+ * by Ronald S. Fearing
+ *
+ * v.beta
+ *
+ * Revisions:
+ *  Ronald S. Fearing   2012-12-19  Initial release
+ */
 
-#include "led.h"
-#include "stopwatch.h"
+#include "utils.h"
 
-// n number of blinks, k number of milliseconds
 void blink_leds(int n, int k)
-{ int i,j;
-	for (i = 0; i < n; i++)
-	{ for (j = 0; j < 10*k; j++)
-	   { LED_RED = 0;
-	     LED_GREEN = 1;
-	     LED_BLUE = 0;
-		swatchDelayUs(100); /* wait 100 us */
-	   }
-	   for (j = 0; j < 10*k; j++)
-	  { LED_RED = 1;
-	     LED_GREEN = 0;
-	     LED_BLUE = 1;
-		swatchDelayUs(100); /* wait 100 us */
-	   }
- 	   LED_RED = 0;
-	   LED_GREEN = 0;
-	   LED_BLUE = 0;
+{
+    int i,j;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < 10*k; j++)
+        {
+            LED_1 = 0; LED_2 = 1; LED_3 = 0;
+            delay_us(100);
+        }
+
+        for (j = 0; j < 10*k; j++)
+        {
+            LED_1 = 1; LED_2 = 0; LED_3 = 1;
+            delay_us(100);
+        }
+        LED_1 = 0; LED_2 = 0; LED_3 = 0;
     }
 }
