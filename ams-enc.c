6edd952e6e3a9a4ab4e797e5b8ec9af9ab7c0daa
Duncan Haldane <dhaldane@berkeley.edu>
2013-07-04 16:27:47 -0700

    Updated setpoints for current robot. Should really be in robot specific h file

--- ams-enc.c
+++ ams-enc.c
@@ -73,8 +73,8 @@ EncObj encPos[NUM_ENC];
 
 #define AMS_ENC_ANGLE_REG   0xFE
 
-#define AMS_ENC_OFFSET_0 823
-#define AMS_ENC_OFFSET_1 3523
+#define AMS_ENC_OFFSET_0 5758
+#define AMS_ENC_OFFSET_1 7706
 
 volatile unsigned char  state = AMS_ENC_IDLE;
 volatile unsigned char  encoder_number = 0;
