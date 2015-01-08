acc100492fb7870c416645c64136a406bd6d6b60
Andrew Pullin <pullin@berkeley.edu>
2015-01-08 14:02:02 -0800

    New version of prebuild script, to work with imageproc-settings migration. Update for cleaner extraction of ident str

--- version.c
+++ version.c
@@ -2,7 +2,7 @@
 
 #include "version-string.h" //This will define the version string, and VERSION_STRING macro
 #ifndef VERSION_STRING
-#error "No version string is defined. This is strictly required to build. Check pre-build python step, version.py ."
+#error "No version string is defined. This is strictly required to build. Check pre-build python step, prebuild.py and setting.h ."
 #else
 static char version[] = VERSION_STRING ;
 #endif
