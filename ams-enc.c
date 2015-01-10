542cc2d19591c1e9cfe3add8feb3bc8825a9a948
Andrew Pullin <pullin@berkeley.edu>
2015-01-09 16:20:06 -0800

    Removed hard-coded AMS offsets from ams-enc.c

--- ams-enc.c
+++ ams-enc.c
@@ -49,6 +49,7 @@
 #include "i2c.h"
 #include "ams-enc.h"
 #include "utils.h"
+#include "settings.h"
 
 
 #define LSB2ENCDEG      0.0219
@@ -73,8 +74,13 @@ EncObj encPos[NUM_ENC];
 
 #define AMS_ENC_ANGLE_REG   0xFE
 
-#define AMS_ENC_OFFSET_0 5758
-#define AMS_ENC_OFFSET_1 7706
+#ifndef AMS_ENC_OFFSET_0
+#define AMS_ENC_OFFSET_0 0
+#endif
+
+#ifndef AMS_ENC_OFFSET_1
+#define AMS_ENC_OFFSET_1 0
+#endif
 
 volatile unsigned char  state = AMS_ENC_IDLE;
 volatile unsigned char  encoder_number = 0;
